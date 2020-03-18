69e5bb8f04e76ae48642747639fb2197c02cd7c7
Joel Bienias <j.bienias@web.de>
2020-03-18 19:24:59 +0100

    add gtk builder hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,46 @@
-#include <iostream>
-#include "Graphics/display.h"
+#include <gtk/gtk.h>
 
-#include <chrono>
-#include <thread>
+GtkWidget *g_lbl_hello;
+GtkWidget *g_lbl_count;
 
-int main()
+int main(int argc, char *argv[])
 {
-	Display display(1280, 720, "Test Window");
+    GtkBuilder      *builder;
+    GtkWidget       *window;
 
-	while(!display.isClosed())
-	{
-		display.clear(1.0f, 0.0f, 0.0f, 1.0f);
+    gtk_init(&argc, &argv);
 
-		display.update();
-	}
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "./Unsaved 1.glade", NULL);
 
-	return 0;
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    // get pointers to the two labels
+    g_lbl_hello = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_hello"));
+    g_lbl_count = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_count"));
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
 }
+
+// called when button is clicked
+void on_btn_hello_clicked()
+{
+    static unsigned int count = 0;
+    char str_count[30] = {0};
+
+    gtk_label_set_text(GTK_LABEL(g_lbl_hello), "Hello, world!");
+    count++;
+    sprintf(str_count, "%d", count);
+    gtk_label_set_text(GTK_LABEL(g_lbl_count), str_count);
+}
+
+// called when window is closed
+void on_window_main_destroy()
+{
+    gtk_main_quit();
